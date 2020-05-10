4fc7c8922222d4fa0f59d1e61b0adf3cffe5a0ce
neeerajtk <neerajtk560@gmail.com>
2020-05-10 23:01:39 +0530

    basic form added

---
+++ todo/src/TodoForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+
+function TodoForm(props){
+    return(
+        <Paper>
+            <TextField/>
+        </Paper>
+    )
+}
+export default TodoForm;
