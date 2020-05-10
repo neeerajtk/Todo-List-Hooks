513ff3291d1e22e084337994d1996da3776fb5a7
neeerajtk <neerajtk560@gmail.com>
2020-05-10 23:50:44 +0530

    dependecy update delete added

--- todo/src/Todo.js
+++ todo/src/Todo.js
@@ -2,6 +2,9 @@ import React from "react";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 function Todo({task,completed}){
     return (
         <ListItem>
@@ -9,6 +12,11 @@ function Todo({task,completed}){
         <ListItemText>
             {task}
         </ListItemText>
+        <ListItemSecondaryAction>
+            <IconButton>
+                <DeleteIcon/>
+            </IconButton>
+        </ListItemSecondaryAction>
     </ListItem>
     );
 }
