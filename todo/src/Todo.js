ee5d1e8eb0a9dbf242840f79ea5ac92cd4fd1dec
neeerajtk <neerajtk560@gmail.com>
2020-05-10 23:55:40 +0530

    edit func added

--- todo/src/Todo.js
+++ todo/src/Todo.js
@@ -4,17 +4,21 @@ import ListItemText from "@material-ui/core/ListItemText";
 import Checkbox from "@material-ui/core/Checkbox";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 function Todo({task,completed}){
     return (
         <ListItem>
         <Checkbox tabIndex={-1} checked={completed}/>
-        <ListItemText>
+        <ListItemText style={{textDecoration: completed?"line-through":"none"}}>
             {task}
         </ListItemText>
         <ListItemSecondaryAction>
             <IconButton>
-                <DeleteIcon/>
+                <DeleteIcon aria-label="Delete"/>
+            </IconButton> 
+            <IconButton>
+                <EditIcon aria-label="Edit"/>
             </IconButton>
         </ListItemSecondaryAction>
     </ListItem>
