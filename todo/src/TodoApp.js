5ab368762d5a043fe42cfdfd570404815d0f4c90
neeerajtk <neerajtk560@gmail.com>
2020-05-10 22:30:19 +0530

    maetrial bg added

--- todo/src/TodoApp.js
+++ todo/src/TodoApp.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
 
 function TodoApp(){
-    return <h1>Todos</h1>
+    return (
+        <Paper 
+            style={{
+                padding:0,
+                margin:0,
+                height:"100vh",
+                backgroundColor:"#fafafa"
+            }}
+            elevation={0}
+        />
+    );
 }
 
 export default TodoApp;
