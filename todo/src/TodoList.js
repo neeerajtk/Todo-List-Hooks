fb81aacc45c0d7a0f8d204e55017ac50699b77a3
neeerajtk <neerajtk560@gmail.com>
2020-05-11 11:53:11 +0530

    style tweaks

--- todo/src/TodoList.js
+++ todo/src/TodoList.js
@@ -9,8 +9,8 @@ function TodoList({todos, removeTodo, toggleTodo, editTodo}){
     return(
     <Paper>
         <List>
-            {todos.map(todo=>(
-                
+            {todos.map((todo,i)=>(
+                <>
                 <Todo 
                 id={todo.id}
                 task={todo.task} 
@@ -19,7 +19,9 @@ function TodoList({todos, removeTodo, toggleTodo, editTodo}){
                 removeTodo={removeTodo}
                 toggleTodo={toggleTodo}
                 editTodo={editTodo}
-                />                
+                />      
+                {i < todos.length - 1 && <Divider/>}
+                </>          
             ))}
         </List>
     </Paper>
