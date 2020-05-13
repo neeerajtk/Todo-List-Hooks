46a661f411e8c0e06e690d4b703c35b06509e864
neeerajtk <neerajtk560@gmail.com>
2020-05-13 20:19:13 +0530

    context update

--- todo/src/context/todos.context.js
+++ todo/src/context/todos.context.js
@@ -1,15 +1,16 @@
-import React, { createContext } from "react";
+import React, { createContext, useReducer } from "react";
+import todoReducer from "../reducers/todo.reducer.js";
 import useTodoState from "../hooks/useTodoState";
 const defaultTodos = [
-  { id: 1, task: "Wash bike", completed: false },
-  { id: 2, task: "Lube bike", completed: true }
+  { id: 1, task: "wash bike", completed: false },
+  { id: 2, task: "lube bike", completed: true }
 ];
 export const TodosContext = createContext();
 
 export function TodosProvider(props) {
-  const todosStuff = useTodoState(defaultTodos);
+  const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
   return (
-    <TodosContext.Provider value={todosStuff}>
+    <TodosContext.Provider value={{ todos, dispatch }}>
       {props.children}
     </TodosContext.Provider>
   );
